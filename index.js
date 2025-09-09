7e51de3331a598c2f249bf69a2f3fb371bd5e398
Bildad <bildadgitoga53@gmail.com>
2025-09-09 18:24:38 +0300

    adding btn trigger in Home.html

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const images = document.querySelectorAll('.images-grid img');
-let index = 0;
+const scheduleNav = document.getElementById('schedule-nav');
+const bookTripBtn = document.querySelectorAll('.tripBtn');
+const discoverBtn = document.getElementById('discoverBtn');
 
+// schudule page button trigger
+scheduleNav.addEventListener('click', () => {
+  window.location.href = 'schedule.html';
+})
+
+// book trip button trigger
+bookTripBtn.forEach(btn => {
+  btn.addEventListener("click", () => {
+    window.location.href = 'schedule.html';
+  })
+})
+
+// discover button trigger
+discoverBtn.addEventListener('click', () => {
+  window.location.href = 'explore.html';
+})
+
+// image rotation logic
+let index = 0;
 function rotateImages() {
   // remove active class from all
   images.forEach(img => img.classList.remove('active'));
